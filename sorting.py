de28ce8a8520d8c8172e9229e00c518c28b5d266
06mkal <283166@vutbr.cz>
2026-04-22 09:44:58 +0200

    bubble sort

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -20,15 +21,25 @@ def selection_sort(values):
 
 def bubble_sort(values):
 
+    plt.ion()
+    plt.show()
     for j in range(len(values)):
         for i in range(len(values)-1):
             if values[i] > values[i+1]:
                 values[i], values[i+1] = values[i+1], values[i]
+                index_highlight1 = j
+                index_highlight2 = j + 1
+                colors = ["steelblue"] * len(values)
+                colors[index_highlight1] = "tomato"
+                colors[index_highlight2] = "tomato"
+                plt.clf()
+                plt.bar(range(len(values)), values, color=colors)
+                plt.title("Bubble Sort")
+                plt.pause(0.1)
         print(values)
+    plt.ioff()
+    plt.show()
     return (values)
-
-
-
 
 
 
